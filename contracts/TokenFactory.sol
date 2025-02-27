255ca85253a50a78026f2cdf0edfb887aabdda4b
amit-katyal <amitkatyal097@gmail.com>
2025-02-27 15:41:19 +0530

    Implement meme token creation in TokenFactory with metadata storage

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -2,16 +2,38 @@
 pragma solidity ^0.8.24;
 
 import "./Token.sol";
+import "hardhat/console.sol";
 
 contract TokenFactory {
+    struct memeToken {
+        string name;
+        string symbol;
+        string description;
+        string tokenImageUrl;
+    }
+
     uint constant DECIMALS = 10 ** 18;
     uint constant MAX_SUPPLY = 1000000 * DECIMALS;
     uint constant INIT_SUPPLY = (20 * MAX_SUPPLY) / 100;
+
+    mapping(address => memeToken) addressToMemeTokenMapping;
 
     function createMemeToken(
         string memory name,
         string memory symbol,
         string memory description,
         string memory imageUrl
-    ) public returns (address) {}
+    ) public returns (address) {
+        Token memeTokenCt = new Token(name, symbol, INIT_SUPPLY);
+        address memeTokenAddress = address(memeTokenCt);
+        memeToken memory newlyCreatedToken = memeToken(
+            name,
+            symbol,
+            description,
+            imageUrl
+        );
+        addressToMemeTokenMapping[memeTokenAddress] = newlyCreatedToken;
+        console.log(memeTokenAddress);
+        return memeTokenAddress;
+    }
 }
