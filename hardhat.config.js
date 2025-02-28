be1990ecfeed149ea7a3dabe813851594e743c75
amit-katyal <amitkatyal097@gmail.com>
2025-02-28 15:56:35 +0000

    Add dotenv support for environment variables in Hardhat configuration; update `package.json` and `package-lock.json`

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ETH_MAINNET_RPC_URL
+      },
+      chainId: 1,
+    }
+  }
 };
